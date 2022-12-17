5609b59690a85792b4d1b3ace33cd93d6f15c794
Jiuzhi Yu <jiuzhiy@andrew.cmu.edu>
2022-12-16 20:22:19 -0500

    Add benchmark

---
+++ openmp_benchmark.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+export OMP_NUM_THREADS=1
+
+echo "1 threads"
+
+for i in 1 2 3
+do
+  for j in 750 1500 3000 4500 6000
+  do
+    echo $j
+    ./src/gradient_domain ./img/${j}_ambient.jpg ./img/${j}_flash.jpg 10 0.5 2 2 0.0005 500
+  done
+done
+
+# export OMP_NUM_THREADS=2
+
+# echo "2 threads"
+
+# for i in 1 2 3
+# do
+#   for j in 750 1500 3000 4500 6000
+#   do
+#     echo $j
+#     ./src/gradient_domain ./img/${j}_ambient.jpg ./img/${j}_flash.jpg 10 0.5 2 2 0.0005 500
+#   done
+# done
+
+# export OMP_NUM_THREADS=4
+
+# echo "4 threads"
+
+# for i in 1 2 3
+# do
+#   for j in 750 1500 3000 4500 6000
+#   do
+#     echo $j
+#     ./src/gradient_domain ./img/${j}_ambient.jpg ./img/${j}_flash.jpg 10 0.5 2 2 0.0005 500
+#   done
+# done
+
+export OMP_NUM_THREADS=8
+
+echo "8 threads"
+
+for i in 1 2 3
+do
+  for j in 750 1500 3000 4500 6000
+  do
+    echo $j
+    ./src/gradient_domain ./img/${j}_ambient.jpg ./img/${j}_flash.jpg 10 0.5 2 2 0.0005 500
+  done
+done
+
+export OMP_NUM_THREADS=16
+
+# echo "16 threads"
+
+# for i in 1 2 3
+# do
+#   for j in 750 1500 3000 4500 6000
+#   do
+#     echo $j
+#     ./src/gradient_domain ./img/${j}_ambient.jpg ./img/${j}_flash.jpg 10 0.5 2 2 0.0005 500
+#   done
+# done
